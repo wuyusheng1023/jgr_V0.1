66be041db372d0983b79c17f66010ed87414490f
Yusheng Wu <wuyusheng1023@gmail.com>
2021-02-04 16:28:37 +0200

    layout frame

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css'
+
+import Row from 'antd/lib/row';
+import Col from 'antd/lib/col';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row>
+        <Col span={18} offset={3} >
+          header
+        </Col>
+      </Row>
+      <Row>
+        <Col span={18} offset={3}>
+          header
+        </Col>
+      </Row>
+      <Row>
+        <Col span={6} offset={3}>
+          sider
+        </Col>
+        <Col span={12} offset={0}>
+          contene
+        </Col>
+      </Row>
+      <Row>
+        <Col span={18} offset={3}>
+          footer
+        </Col>
+      </Row>
     </div>
   );
 }
